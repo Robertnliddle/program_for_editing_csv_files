fa9994bd563d689c613f6bbe464735c2131dc290
Robertliddle <Robertnliddle@gmail.com>
2023-10-12 23:05:35 +0200

    Initial commit

---
+++ functions.py
@@ -0,0 +1,39 @@
+import csv
+import sys
+
+
+def read_csv_file(src):
+    table = []
+    with open("src", "r") as f:
+        reader = csv.reader(f)
+        for row in reader:
+            table.append(row)
+        return table
+
+
+def display_table(table, sep="-"):
+    print_template = " | {:<10.10s} | {:<10.10s} | {:<15.15s} |"
+    print(print_template.format(table[0][0], table[0][1], table[0][2]))
+    print(41 * "-")
+    for row in table[1:]:
+        print(print_template.format(row[0], row[2], row[3]))
+    print(41 * "-")
+
+
+def validate_field_section(rdx, cdx, table):
+    if len(table) - 1 < rdx:
+        print("Not enough rows")
+        sys.exit()
+    if len(table[rdx]) - 1 < cdx:
+        print("Not enough columns")
+        sys.exit()
+
+
+def save_csv_file(table, dst):
+    with open(dst, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(table)
+
+# src = sys.argv[1]  # file_path
+# dst = sys.argv[2]  # save_file_path
+# changes = sys.argv[3]
---
+++ reader.py
@@ -0,0 +1,67 @@
+import sys
+import os
+from functions import (
+     read_csv_file,
+     display_table,
+     save_csv_file,
+     validate_field_section,
+)
+# import from functions.py
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("Please provide with another file path")
+        sys.exit()
+
+
+src = sys.argv[1]  # file_path
+dst = sys.argv[2]  # save_file_path
+changes = sys.argv[3:]  # changes
+
+if not dst.endswith('.csv'):
+    print(f"File path not valid")
+    sys.exit()
+
+print("File path:", src)
+
+if not os.path.exists(src) and not os.path.isfile(src):
+    print(f"The path does not lead to a file: {src}")
+    print(f"Available files:")
+    for fn in os.listdir():
+        if fn.endswith(".py") or os.path.isdir(fn):
+            continue
+        print(f" - {fn}")
+        # fn = filename
+else:
+    # reads
+    table = read_csv_file(src)
+    # display
+    display_table(table, sep="*")
+    print_template = " | {:<10.10s} | {:<10.10s} | {:<15.15s} |"
+
+    # load changes
+    if not changes:
+        print("No changes to be made were provided, try again.")
+        sys.exit()
+
+    print()
+
+    for chg in changes:
+        print(chg)
+        rdx, cdx, val = chg.split(';')
+        rdx = int(rdx)
+        cdx = int(cdx)
+        validate_field_section(rdx, cdx, table)
+        value_to_be_changed = table[rdx][cdx]
+        print(f"Changing {value_to_be_changed} into {val}")
+        table[rdx][cdx] = val
+        # counts like this 0, 1, 2, 3 not from 1 (banners counts as 0 if you have one)
+
+    display_table(table, sep="*")
+
+    save_csv_file(table, dst)
+
+
+if __name__ == '__main__':
+    main()
